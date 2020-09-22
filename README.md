adcc74d563ad4f4c288c45039ca33fb84be3c73d
Justin Lansdale <justinlansdale@Justins-MacBook-Pro.local>
2020-09-22 16:56:51 -0600

    modified readme

--- README.md
+++ README.md
@@ -21,29 +21,33 @@ For normal everyday people, reading contracts is not the most appealing endeavor
 <img src="images/work_flow.png"  height="100" width="500" />
 </p>
 
+</br>
+
 ### Creating the Database
 
 Contracts are typically private documents and therefore obtaining enough contracts to genereate a model was no easy task.  I obtained all of my contracts for this model from the City of Chicago portal (https://data.cityofchicago.org).  Below is the workflow used to obtain these documents
+</br>
 
 <p align="center">
 <img src="images/scrape_workflow.png"  height="400" width="700" />
 </p>
-
+</br>
 
 ### Processing the Contracts
 
 These contracts came in as mostly scanned pdfs, so in order to process them I used Optical Character Recognition (OCR).  Below is an example of of the types of .pdf files that I obtained.  
-
+</br>
 <p align="center">
 <img src="images/example_pdfs.png"  height="300" width="700" />
 </p>
+</br>
 
-There were a total of six contract classes from the City of Chicago, detailed below.
-
+### The Six Contract Classifications
+</br>
 <p align="center">
 <img src="images/contract_class_table.png"  height="300" width="700" />
 </p>
-
+</br>
 
 OCR is computationally expensive so in order to optimize my time, I used cloud computing on AWS.  I split the 3600 pdfs I was going to use for the model evenly between six m5a.8xlarge EC2 instances.  Each instance generated a .jpg image file for each page of the .pdf files. I then split these .jpg images onto the 32 cores and used OCR to process the images individually in order to reduce processing time.  These returned individual .txt files for each image, which I combined to create one .txt file for the original .pdf.  The process flow for this is displayed in the image below.
 
@@ -54,13 +58,47 @@ OCR is computationally expensive so in order to optimize my time, I used cloud c
 
 ### Generating the models
 
-In order to generate the models, I needed to perform a proper train-test-split, stratifying the text files from each class evenly, utilizing the folders as class names and generating new folders for the train, validate, and holdout sets.  The workflow for this is highlighted below.
+In order to generate the models, I needed to perform a proper train-test-split, stratifying the text files from each class evenly, utilizing the folders as class names and generating new folders for the train, validate, and holdout sets.  I ended up with 337 train documents, 113 test documents, and 150 holdout documents.  The workflow for this is highlighted below.
 
 <p align="center">
 <img src="images/train_test_split_workflow.png"  height="400" width="700" />
 </p>
 
+## Naive Bayes Classifier
 
-## The Contracts
+I started off by using a Naive Bayes Classifier model.  The initial model, using a single word to generate the tfidf matrix gave an accuracy score of 86.2% and after tuning the model using a 4 word grouping the model achieved an accruacy of 90.1%.
 
-Contracts are typically private documents and therefore obtaining enough contracts to genereate a model was no easy task.  
+</br>
+</br>
+</br>
+
+
+### Class Report | Single Word
+|           |   Architectural Engineering |   Commodities |   Comptroller |   Construction |   Delegate Agency |   Professional Services |   accuracy |   macro avg |   weighted avg |
+|:----------|----------------------------:|--------------:|--------------:|---------------:|------------------:|------------------------:|-----------:|------------:|---------------:|
+| precision |                    0.834395 |      0.953271 |      0.962121 |       0.922581 |          0.953642 |                0.651515 |   0.862222 |    0.879588 |       0.879588 |
+| recall    |                    0.873333 |      0.68     |      0.846667 |       0.953333 |          0.96     |                0.86     |   0.862222 |    0.862222 |       0.862222 |
+| f1-score  |                    0.85342  |      0.793774 |      0.900709 |       0.937705 |          0.956811 |                0.741379 |   0.862222 |    0.863966 |       0.863966 |
+| support   |                  150        |    150        |    150        |     150        |        150        |              150        |   0.862222 |  900        |     900        |
+
+</br>
+</br>
+</br>
+
+
+### Class Report | Single 4 Words
+|           |   Architectural Engineering |   Commodities |   Comptroller |   Construction |   Delegate Agency |   Professional Services |   accuracy |   macro avg |   weighted avg |
+|:----------|----------------------------:|--------------:|--------------:|---------------:|------------------:|------------------------:|-----------:|------------:|---------------:|
+| precision |                    0.916084 |      0.858108 |      0.92053  |       0.90303  |          0.972973 |                0.834483 |   0.901111 |    0.900868 |       0.900868 |
+| recall    |                    0.873333 |      0.846667 |      0.926667 |       0.993333 |          0.96     |                0.806667 |   0.901111 |    0.901111 |       0.901111 |
+| f1-score  |                    0.894198 |      0.852349 |      0.923588 |       0.946032 |          0.966443 |                0.820339 |   0.901111 |    0.900491 |       0.900491 |
+| support   |                  150        |    150        |    150        |     150        |        150        |              150        |   0.901111 |  900        |     900        |
+
+</br>
+</br>
+
+Below is the confusion matrix generated from the 4-word model.
+</br>
+<p align="center">
+<img src="images/train_test_split_workflow.png"  height="400" width="700" />
+</p>
--- src/naive_bayes_model.py
+++ src/naive_bayes_model.py
@@ -28,17 +28,110 @@ plt.style.use('tableau-colorblind10')
 
 class NaiveBayes():
 
-    def __init__(self, train_targets, tfidf_matrix, count_vec_object):
+    def __init__(self, train_dir, test_dir, stop_words, lemmatize=False, 
+                 stem=False):
         """
-        Params
-        tfidf_matrix: matrix with tfidf performed on it
-        cv: count vectorizer object
-        """
-        self.train_targets = train_targets
-        self.tfidf_matrix = tfidf_matrix
-        self.cv = count_vec_object
+        Instantiates a RandomForest object and instantiates ContractPipelines
+        for each directory
         
+        Params:
+        train_dir: str directory where the training set is held must 
+        end with '/'
+        test_dir:  str directory where the testing set is held must
+        end with '/'
+        houldout_dir:  str directory where the holdout set is held must 
+        end with '/'
+        """
 
+        self.train_dir = train_dir
+        self.test_dir = test_dir
+        self.stop_words = stop_words
+        self.train_pipe = ContractPipeline(self.train_dir, self.stop_words)
+        self.test_pipe = ContractPipeline(self.test_dir, self.stop_words)
+        self.model = None
+        self.lemmatize = lemmatize
+        self.stem = stem
+
+    def convert_txt_to_lists(self):
+        """
+        If the documents are in .txt format use this to call the 
+        get_list_of_txts on each of the directorys
+        """
+        
+        self.train_pipe.get_list_of_txts()
+        self.test_pipe.get_list_of_txts()
+    
+    def convert_doc_to_lists(self):
+        """
+        If the documents are in .doc format use this to call the 
+        get_list_of_txts on each of the directorys
+        """
+        
+        self.train_pipe.get_list_of_docs()
+        self.test_pipe.get_list_of_docs()
+
+    def remove_stop_words(self):
+        """
+        Runs the remove_stop_words method on each of the piplines
+        """
+        
+        self.train_pipe.remove_stop_words()
+        self.test_pipe.remove_stop_words()
+    
+    def condense(self):
+        """
+        Runs the word_condenser method on each of the pipelines
+        """
+        
+        self.train_pipe.word_condenser()
+        self.test_pipe.word_condenser()
+    
+    def tfidf(self, stem=False, lemmatize=False, max_features=None, 
+              ngram_range=(1,1)):
+        """
+        Calls the tf_vect method on each of the piplelines. It uses the original
+        string with stop words removed.  If stem == True then it uses a stemmed
+        version of the string and if lemmatize == True then it uses a lemmatized
+        version fo the string.
+
+        Params
+        stem: bool default is false
+        lemmatize: bool default is false
+        """
+        
+        if not stem and not lemmatize:
+            self.train_pipe.tf_vect(self.train_pipe.stops_removed_str,
+                                    max_features=max_features, 
+                                    ngram_range=ngram_range)
+            self.test_pipe.tf_vect(self.test_pipe.stops_removed_str,
+                                   max_features=max_features, 
+                                   ngram_range=ngram_range)
+            
+        elif self.stem:
+            self.condense()
+            self.train_pipe.tf_vect(self.train_pipe.porter_str,
+                                    max_features=max_features, 
+                                    ngram_range=ngram_range)
+            self.test_pipe.tf_vect(self.test_pipe.porter_str,
+                                   max_features=max_features, 
+                                   ngram_range=ngram_range)
+            
+        elif self.lemmatize:
+            self.condense()
+            self.train_pipe.tf_vect(self.train_pipe.wordnet_str,
+                                    max_features=max_features, 
+                                    ngram_range=ngram_range)
+            self.test_pipe.tf_vect(self.test_pipe.wordnet_str,
+                                   max_features=max_features, 
+                                   ngram_range=ngram_range)
+        else:
+            print('Cannot lemmatize and stem')
+        
+    def vectorizer(self, test=False, ngram_range=(1,1)):
+        self.train_pipe.count_vectorizer(ngram_range=ngram_range)
+        
+        if test:
+            self.test_pipe.count_vectorizer(ngram_range=ngram_range)
 
     def naive_bayes_model(self):
         """
@@ -54,7 +147,7 @@ class NaiveBayes():
         cv: CountVectorizer object for the documents
         """
         self.nb_model = MultinomialNB()
-        self.nb_model.fit(self.tfidf_matrix, self.train_targets)
+        self.nb_model.fit(self.train_pipe.tfidf, self.train_pipe.target_lst)
         
 
     def return_top_n_words(self,  n=7):
@@ -69,7 +162,7 @@ class NaiveBayes():
         technique: technique: str choose from ['porter', 'snowball','wordnet']
 
         """
-        feature_words = self.cv.get_feature_names()
+        feature_words = self.train_pipe.cv.get_feature_names()
         categories = self.nb_model.classes_
         self.top_words_dic = {}
         for cat in range(len(categories)):
@@ -82,8 +175,7 @@ class NaiveBayes():
             
         
 
-    def get_accuracy_classification_report(self, train_docs, test_docs, 
-                                           test_targets, ngram_range=(1,1)):
+    def get_accuracy_classification_report(self, ngram_range=(1,1)):
         """
         Prints out and returns the accuracy score from the prediction vs the actuals
         for the test set
@@ -100,30 +192,67 @@ class NaiveBayes():
                             ('tfidf', TfidfTransformer()),
                             ('model', MultinomialNB()),
                             ])
-
-        self.nb_pipeline.fit(train_docs, self.train_targets)
-        self.predicted = self.nb_pipeline.predict(test_docs)
-        self.accuracy = np.mean(self.predicted == test_targets)
+        
+        if not self.stem and not self.lemmatize:
+            X_train = self.train_pipe.stops_removed_str
+            X_test = self.test_pipe.stops_removed_str
+        
+        elif self.lemmatize:
+            self.condense()
+            X_train = self.train_pipe.wordnet_str
+            X_test = self.test_pipe.wordnet_str
+        
+        elif self.stem:
+            self.condense()
+            X_train = self.train_pipe.porter_str
+            X_test = self.test_pipe.porter_str
+        
+        else:
+            print('Cannot lemmatize and stem')
+        
+        
+        self.nb_pipeline.fit(X_train, self.train_pipe.target_lst)
+        print(len(X_test))
+        self.predicted = self.nb_pipeline.predict(X_test)
+        print(self.predicted.shape)
+        print(len(self.test_pipe.target_lst))
+        self.accuracy = np.mean(self.predicted == self.test_pipe.target_lst)
         print("\nThe accuracy on the test set is {0:0.3f}.".format(self.accuracy))
-        self.class_report = classification_report(test_targets, self.predicted, digits=3,output_dict=True)
+
+        self.class_report = classification_report(self.test_pipe.target_lst, 
+                                                  self.predicted,
+                                                  digits=3, output_dict=True)
     
     def pickle_model(self):
+        """
+        Pickles the model
+        """
+
         with open('nb_pickle.pkl', 'wb') as f:
             pickle.dump(self.nb_pipeline, f)
 
-    def confusion_matrix_plot(self, test_docs, test_targets, ax):
+    def confusion_matrix_plot(self, ax):
         """
         Generates a confusion matrix
 
         Params
-        test_docs: list of strings from the test set
-        test_targets: list test target strings associated with the test_docs
         ax: axes to be used for the plot
         """
-        plot_confusion_matrix(self.nb_pipeline, test_docs, test_targets, xticks_rotation='vertical',
+        if not self.stem and not self.lemmatize:
+            X_test = self.test_pipe.stops_removed_str
+        
+        elif self.lemmatize:
+            X_test = self.test_pipe.wordnet_str
+        
+        elif self.stem:
+            X_test = self.test_pipe.porter_str
+        
+        else:
+            print('Cannot lemmatize and stem')
+        
+        plot_confusion_matrix(self.nb_pipeline, X_test, self.test_pipe.target_lst, 
+                              xticks_rotation='vertical',
                               cmap=plt.cm.Blues, ax=ax)
-
-
 
 
 if __name__ == "__main__":
@@ -132,7 +261,7 @@ if __name__ == "__main__":
 contractTxts/TrainTestHoldout/TrainTest/TrainDocs/'
     test_dir = '/Users/justinlansdale/Documents/Galvanize/Capstone2/\
 contractTxts/TrainTestHoldout/TestDocs/'
-    holdout_dir = ''
+    
     start = time.time()
 
     stop_words = stopwords.words('english')
@@ -140,38 +269,25 @@ contractTxts/TrainTestHoldout/TestDocs/'
                    'number', 'may']
     stop_words.extend(extra_stops)
 
-    train_pipe = ContractPipeline(train_dir, stop_words)
-    test_pipe = ContractPipeline(test_dir, stop_words)
-    
-    train_pipe.get_list_of_txts()
-    test_pipe.get_list_of_txts()
-
-    train_pipe.remove_stop_words()
-    test_pipe.remove_stop_words()
-
-    train_pipe.ngram_tf_vect(train_pipe.stops_removed_str, max_features=2000, ngram_range=(3,3))
-
-    count_matrix, tfidf_matrix, cv = (train_pipe.tf_idf_matrix(train_pipe.
-                                                               stops_removed_str,
-                                                               max_features=2000))
-    
-    nb = NaiveBayes(train_pipe.target_lst, train_pipe.tfidf, cv)
-    nb.naive_bayes_model()
-    nb.return_top_n_words(n=4)
-
-    nb.get_accuracy_classification_report(train_pipe.stops_removed_str,
-                                          test_pipe.stops_removed_str,
-                                          test_pipe.target_lst, ngram_range=(4,4))
-    
-    
-    d = {'True Label': test_pipe.target_lst, 
-        'Predicted Label': nb.nb_pipeline.predict(test_pipe.stops_removed_str)}
-    
-    df = pd.DataFrame(d)
-    df.to_csv('../data/optimized_predictions.csv')
-
-
-
+    nb = NaiveBayes(train_dir, test_dir, stop_words)
+    nb.convert_txt_to_lists()
+    nb.remove_stop_words()
+    # nb.tfidf(ngram_range=(4,4))
+    # nb.vectorizer(ngram_range=(4,4))
+    # nb.naive_bayes_model()
+    # nb.return_top_n_words()
+    nb.get_accuracy_classification_report(ngram_range=(4,4))
+    nb.pickle_model()
+    # fig, ax = plt.subplots(figsize=(12,12))
+    # plt.rcParams.update({'font.size': 20})
+    # nb.confusion_matrix_plot(ax)
+    # ax.set_xlabel('Predicted Label', fontsize=20, weight='bold')
+    # ax.set_ylabel('True Label', fontsize=20, weight='bold')
+    # ax.set_title('Confusion Matrix', fontsize=30, weight='bold')
+    # plt.xticks(fontsize=16, rotation=70, weight='bold')
+    # plt.yticks(fontsize=16, weight='bold')
+    # plt.tight_layout()
+    # plt.show()
 
     # ngrams = []
     # max_grams = 7
@@ -183,26 +299,12 @@ contractTxts/TrainTestHoldout/TestDocs/'
     
     # for ngram in ngrams:
     #     print(f'Generating nb model with {ngram} range')
-    #     nb.get_accuracy_classification_report(train_pipe.stops_removed_str,
-    #                                       test_pipe.stops_removed_str,
-    #                                       test_pipe.target_lst, ngram_range=ngram)     
+    #     nb.get_accuracy_classification_report(ngram_range=ngram)     
     #     y.append(nb.accuracy)
     
-
-
-
-    # fig, ax = plt.subplots(figsize=(12,12))
-    # plt.rcParams.update({'font.size': 20})
-    # nb.confusion_matrix_plot(test_pipe.stops_removed_str, test_pipe.target_lst, ax)
-    # ax.set_xlabel('Predicted Label', fontsize=20, weight='bold')
-    # ax.set_ylabel('True Label', fontsize=20, weight='bold')
-    # ax.set_title('Confusion Matrix', fontsize=30, weight='bold')
-    # plt.xticks(fontsize=16, rotation=70, weight='bold')
-    # plt.yticks(fontsize=16, weight='bold')
-    # plt.tight_layout()
-    # plt.show()
-    
-    print(nb.class_report)
+    d = nb.class_report
+    df = pd.DataFrame(d)
+    print(df.to_markdown())
     end = time.time()
     print(f'This took {end-start:.2f}')
 
